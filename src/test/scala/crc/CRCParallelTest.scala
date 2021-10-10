9566a5fe893a33b280cd8da02f72643dcc44a343
Banson Xu <xzcxzcyy@126.com>
2021-10-10 18:01:07 +0800

    emit verilog

--- src/test/scala/crc/CRCParallelTest.scala
+++ src/test/scala/crc/CRCParallelTest.scala
@@ -9,6 +9,7 @@ import org.scalatest.Matchers
 class CRCParallelTest extends FreeSpec with ChiselScalatestTester with Matchers {
   "CRCParallel should pass" in {
     test(new CRCParallel(3, 29)) { c =>
+      (new chisel3.stage.ChiselStage).emitVerilog(new CRCParallel(3, 29), Array("--target-dir", "generated/CRCParallel"))
       c.io.in.poke(utils.Bits.asBinary("110").U)
       c.io.out.expect(utils.Bits.asBinary("1001").U)
     }
